7600b98d75e6a5712f6a040cb3250d6cc26c0435
Doku-Dow <shems.alilat@outlook.fr>
2025-01-07 15:20:55 +0100

    updated header

--- simpleshell.h
+++ simpleshell.h
@@ -7,7 +7,18 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stddef.h>
 
-/* to be continued */
+#include "shell.h"
+
+int _putchar(char c);
+void _puts(char *str);
+int _strlen(char *s);
+char *_strdup(char *str);
+char *concat_all(char *name, char *sep, char *value);
+
+char **splitstring(char *str, const char *delim);
+void execute(char **argv);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif

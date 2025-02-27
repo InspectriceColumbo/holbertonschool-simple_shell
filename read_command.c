636a6bbc0a76f8cacc3f96635f92df03ac53226b
InspectriceColumbo <9949@holbertonstudents.com>
2025-02-27 15:59:19 +0100

    handled EOF and error reading input

--- read_command.c
+++ read_command.c
@@ -10,14 +10,24 @@ char *read_command(void)
 {
 	size_t bsize = 0; /* getline will dinamically allocate memory */
 	char *line = NULL;
+	ssize_t len;
 
-	/*Read the command from stdin */
-	if (getline(&line, &bsize, stdin) == -1)
+	ssize_t len = getline(&line, &bsize, stdin);/* read command from stdin */
+
+	if (len == -1)/* EOF or error */
 	{
-		perror("Error reading input");
-		free(line);/* End of file condition (Ctrl+D) or error */
-		return (NULL);
+		if (line == NULL)/* if no input is read */
+		{
+			return (NULL);
+		}
+		else
+		{
+			perror("Error reading input");
+			free(line);
+			return (NULL);/* return NULL or error */
+		}
 	}
 
 	return (line);
+
 }

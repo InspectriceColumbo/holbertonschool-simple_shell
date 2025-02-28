1fd761dd110df103802f65ea6481ab8fa608bc00
InspectriceColumbo <9949@holbertonstudents.com>
2025-02-28 17:29:27 +0100

    added more precision on message

--- execute_command.c
+++ execute_command.c
@@ -28,7 +28,7 @@ int execute_command(char *line)
 	{
 		if (execve(line, args, environ) == -1)/*if execve fails print an error*/
 		{
-			fprintf(stderr, "%s: No such file or directory\n", line);
+			fprintf(stderr, "./hsh: %s: No such file or directory\n", line);
 			return (-1);/*failure code instead of exiting*/
 		}
 	}

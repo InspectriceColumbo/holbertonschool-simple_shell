dea3ad74686a6ce17956ae65c74862702cfd24f0
InspectriceColumbo <9949@holbertonstudents.com>
2025-02-28 18:01:11 +0100

    modified sig handler so that when ctrl c is pressed the shell exits nicely

--- sighandler.c
+++ sighandler.c
@@ -2,14 +2,15 @@
 #include <unistd.h>
 
 /**
- * sig_handler - checks if Ctrl C is pressed
- * @sig_num: int
+ * sig_handler - checks if Ctrl C is pressed and if so, exits the shell
+ * @sig_num: the signal number, type int
  */
 
 void sig_handler(int sig_num)
 {
 	if (sig_num == SIGINT)
 	{
-		write(STDOUT_FILENO, "\n(c$) ", 6);  /* Prints prompt after Ctrl+C */
+		write(STDOUT_FILENO, "\n", 1);
+		exit(0);
 	}
 }

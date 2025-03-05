9a0ab5284f9e482d44dcc9af36a2f27c7a4f2281
InspectriceColumbo <9949@holbertonstudents.com>
2025-03-05 12:55:55 +0100

    added exit built in function to exit shell

---
+++ exit_builtin.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * exit_builtin - Handles the exit built-in command
+ * Return: none (void)
+ */
+
+void exit_builtin(void)
+{
+	exit(0);
+}
